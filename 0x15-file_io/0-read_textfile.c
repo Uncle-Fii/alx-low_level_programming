f99b69d137fc6948d8c1d794ec479736d46c775f
Uncle-Fii <fkankrumah@gmail.com>
2022-04-29 19:28:58 -0700

    adds file 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,31 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
+ * read_textfile - reads a text file and prints it to standard output
+ * @filename: file descripter
+ * @letters: count
  *
- * Return: the number of letters printed, or 0 if it failed
+ * Return: any time size_t
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int s, t;
-	char *buf;
-
-	if (!filename)
-		return (0);
+	int fd, sz, oz;
+	char *buf = malloc(sizeof(char) * letters);
 
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (0);
-
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
-
-	s = read(fd, buf, letters);
-	if (s < 0)
+	if ((filename == NULL) || (fd == -1) || (buf == NULL))
 	{
 		free(buf);
 		return (0);
 	}
-	buf[s] = '\0';
-
+	sz = read(fd, buf, letters);
+	if (sz == -1)
+		return (0);
+	buf[sz] = '\0';
+	oz = write(STDOUT_FILENO, buf, sz);
+	if (oz == -1)
+		return (0);
 	close(fd);
-
-	t = write(STDOUT_FILENO, buf, s);
-	if (t < 0)
-	{
-		free(buf);
-		return (0);
-	}
-
-	free(buf);
-	return (t);
+	return (oz);
 }
